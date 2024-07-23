c8f580e21d0bb7711d68b7bcda899ecc62e45bfa
alex-vm <alex.crownshaw12@gmail.com>
2024-07-23 20:26:41 +0100

    Hardware sample data testing

--- madgwick_firmware/src/main.c
+++ madgwick_firmware/src/main.c
@@ -1,14 +1,40 @@
+#if defined(D_NEXYS_A7)
+   #include <bsp_printf.h>
+   #include <bsp_mem_map.h>
+   #include <bsp_version.h>
+#else
+   PRE_COMPILED_MSG("no platform was defined")
+#endif
+#include <psp_api.h>
+
 #include "madgwick_mem_map.h"
 #include "test_vectors.h"
 
 #define WRITE_ADDR(dir, value) { (*(volatile unsigned *)dir) = (value); }
 #define READ_ADDR(dir) (*(volatile unsigned *)dir)
 
-
 volatile unsigned int ctrl_reg;
 volatile unsigned long q_w, q_x, q_y, q_z;
 
+void get_attitude(unsigned int);
+
+
 int main(void)
+{
+    uartInit();
+
+    int size = sizeof(ax) / sizeof(int);
+    for (int i = 0; i < size; i++)
+    {
+        get_attitude(i);
+        printfNexys("%d,%d,%d,%d", q_w, q_x, q_y, q_z);
+    }
+
+    return 0;
+}
+
+
+void get_attitude(unsigned int index)
 {
     // Reset madgwick accelerator
     ctrl_reg = READ_ADDR(CTRL_REG_ADDR);    // Read control reg
@@ -20,13 +46,13 @@ int main(void)
 
     ctrl_reg = READ_ADDR(CTRL_REG_ADDR);    // Read control reg
 
-    WRITE_ADDR(A_X_REG_ADDR, ax[0]);    // Write accel data
-    WRITE_ADDR(A_Y_REG_ADDR, ay[0]);
-    WRITE_ADDR(A_Z_REG_ADDR, az[0]);
+    WRITE_ADDR(A_X_REG_ADDR, ax[index]);    // Write accel data
+    WRITE_ADDR(A_Y_REG_ADDR, ay[index]);
+    WRITE_ADDR(A_Z_REG_ADDR, az[index]);
 
-    WRITE_ADDR(W_X_REG_ADDR, wx[0]);    // Write gyro rate data
-    WRITE_ADDR(W_Y_REG_ADDR, wy[0]);
-    WRITE_ADDR(W_Z_REG_ADDR, wz[0]);
+    WRITE_ADDR(W_X_REG_ADDR, wx[index]);    // Write gyro rate data
+    WRITE_ADDR(W_Y_REG_ADDR, wy[index]);
+    WRITE_ADDR(W_Z_REG_ADDR, wz[index]);
 
     // Start madgwick computation
     ctrl_reg = READ_ADDR(CTRL_REG_ADDR);    // Read control reg
@@ -43,5 +69,4 @@ int main(void)
     q_z = READ_ADDR(Q_Z_REG_ADDR);
 
     WRITE_ADDR(CTRL_REG_ADDR, (ctrl_reg & ~CTRL_REG_START_MASK));    // De-assert start flag
-
 }
